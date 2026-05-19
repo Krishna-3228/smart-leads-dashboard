76d25754015f4bbabaae30c92fa939a93a92d46d
krishna <tiksgupta20@gmail.com>
2026-05-19 16:54:54 +0530

    implement login authentication flow

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -41,3 +41,45 @@ export const registerUser = async (
     });
   }
 };
+
+export const loginUser = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    res.status(200).json({
+      message: "Login successful",
+      token: generateToken(
+        user._id.toString(),
+        user.role
+      ),
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
